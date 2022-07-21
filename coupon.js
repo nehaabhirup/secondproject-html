208fd85e3cea64917e442ed9d5222cc2ab927b1b
unknown <92681978+nehaabhirup@users.noreply.github.com>
2022-07-21 23:49:08 +0530

    html code

---
+++ coupon.js
@@ -0,0 +1,6 @@
+function loadCoupon(){
+    document.getElementById('coupon').style.visibility = 'visible'
+}
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility = 'hidden'
+}
